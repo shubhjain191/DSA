11920b97592b268c54efe55194815516e3986ba7
Shubh Jain <70976110+shubhjain191@users.noreply.github.com>
2024-07-15 17:27:33 +0530

    Add files via upload

---
+++ Datatypes.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a; //declaration
+    a=12; //intialisation
+
+    cout<<"size of int"<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float"<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char"<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"size of bool"<<sizeof(d)<<endl;
+
+    short int si;
+    long int li;
+    cout<<"size of shortint"<<sizeof(si)<<endl;
+    cout<<"size of longint"<<sizeof(li)<<endl;
+
+    return 0;
+}
+
+/* 
+->> Output <<-
+
+size of int4
+size of float4
+size of char1
+size of bool1
+size of shortint2
+size of longint8
+
+ */
